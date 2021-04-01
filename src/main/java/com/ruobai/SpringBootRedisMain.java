dd066d8fb02ee187ccde3c28bee47328abbb9e03
Administrator <2522100775@qq.com>
2021-04-01 08:28:45 +0800

    first commit

---
+++ src/main/java/com/ruobai/SpringBootRedisMain.java
@@ -0,0 +1,18 @@
+package com.ruobai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+/**
+ * company: www.abc.com
+ * Author: Administrator
+ * Create Data: 2021/3/30 0030
+ */
+@SpringBootApplication
+public class SpringBootRedisMain {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRedisMain.class, args);
+    }
+}
