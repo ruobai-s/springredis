c0599fdd71927ae8e0052169548af847d3191b52
Administrator <2522100775@qq.com>
2021-04-07 12:01:50 +0800

    404 commit

--- src/main/java/com/ruobai/utils/RedisUtil.java
+++ src/main/java/com/ruobai/utils/RedisUtil.java
@@ -1,9 +1,17 @@
 package com.ruobai.utils;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
 /**
  * company: www.abc.com
  * Author: Administrator
  * Create Data: 2021/4/7 0007
  */
+@Component
 public class RedisUtil {
+    @Autowired
+    private RedisTemplate<String,Object> redisTemplate;
+
 }
